161085ac874130a913ec536feba6fc671b368c60
Adam <adam.gebicki01@gmail.com>
2023-04-21 11:51:19 +0200

    myfirstCommit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance of the service collection
+        var services = new ServiceCollection();
+
+        // Register your service(s) here
+        services.AddSingleton<MyService>();
+
+        // Build the service provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Resolve your service(s) here
+        var myService = serviceProvider.GetService<MyService>();
+
+        // Use your service(s) here
+        myService.DoSomething();
+    }
+}
---
+++ aa.cs
@@ -0,0 +1,15 @@
+namespace test
+{
+    public class AAaa
+    {
+        private int _index;
+        
+        public AAaa(){
+            _index = 5;
+        }
+
+        public int MakeItClear(int number){
+            return _index+number;
+        }
+    }
+}
